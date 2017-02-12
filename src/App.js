6955263c8fb16584d72ba07f2e05b2e89467dd37
scottdomes <scottdomes@gmail.com>
2017-02-12 14:17:26 -0800

    Did gifs for flex-wrap, flex-flow, align-content

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 const text = [
-  'justify-content: center; align-items: center;',
-  'justify-content: center; align-items: center;',
-  'align-items: center;',
-  'align-items: stretch;',
-  'align-items: baseline;',
+  'align-content: stretch;',
+  'align-content: flex-start;',
+  'align-content: flex-end;',
+  'align-content: center;',
+  'align-content: space-between;',
+  'align-content: space-around;'
 ]
 
+// const text2 = false
 const text2 = [
-  'flex-direction: row;',
-  'flex-direction: column;'
+  'flex-flow: column wrap;',
+  'flex-flow: column wrap;',
+  'flex-flow: column wrap;',
+  'flex-flow: column wrap;',
+  'flex-flow: column wrap;',
+  'flex-flow: column wrap;'
 ]
 
 class App extends Component {
@@ -20,7 +26,7 @@ class App extends Component {
 
   animate() {
     this.i++
-    if (this.i === 2) {
+    if (this.i === text.length) {
       this.i = 0
     }
     this.setState({ stage: 'stage' + (this.i + 1) }, () => {
@@ -29,6 +35,12 @@ class App extends Component {
         box2: this.getDimensions('two2'),
         box3: this.getDimensions('three2'),
         box4: this.getDimensions('four2'),
+        box5: this.getDimensions('five2'),
+        box6: this.getDimensions('six2'),
+        box7: this.getDimensions('seven2'),
+        box8: this.getDimensions('eight2'),
+        box9: this.getDimensions('nine2'),
+        box10: this.getDimensions('ten2'),
         cont: this.getContainerDimensions(),
       })
     })
@@ -59,6 +71,12 @@ class App extends Component {
       box2: this.getDimensions('two2'),
       box3: this.getDimensions('three2'),
       box4: this.getDimensions('four2'),
+      box5: this.getDimensions('five2'),
+      box6: this.getDimensions('six2'),
+      box7: this.getDimensions('seven2'),
+      box8: this.getDimensions('eight2'),
+      box9: this.getDimensions('nine2'),
+      box10: this.getDimensions('ten2'),
       cont: this.getContainerDimensions()
     })
   }
@@ -66,7 +84,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>{text2[this.i]}</h1>
+        { text2 && <h1>{text2[this.i]}</h1> }
         <h1>{text[this.i]}</h1>
         <div id="app">
           <div id="container" style={this.state.cont}>
@@ -74,12 +92,24 @@ class App extends Component {
             <div id="two" style={this.state.box2} className="square"><p>2</p></div>
             <div id="three" style={this.state.box3} className="square"><p>3</p></div>
             <div id="four" style={this.state.box4} className="square"><p>4</p></div>
+            <div id="five" style={this.state.box5} className="square"><p>5</p></div>
+            <div id="six" style={this.state.box6} className="square"><p>6</p></div>
+            <div id="seven" style={this.state.box7} className="square"><p>7</p></div>
+            <div id="eight" style={this.state.box8} className="square"><p>8</p></div>
+            <div id="nine" style={this.state.box9} className="square"><p>9</p></div>
+            <div id="ten" style={this.state.box10} className="square"><p>10</p></div>
           </div>
           <div id="container2" className={'adjusted ' + this.state.stage}>
             <div id="one2" className="square2"><p>1</p></div>
             <div id="two2" className="square2"><p>2</p></div>
             <div id="three2" className="square2"><p>3</p></div>
             <div id="four2" className="square2"><p>4</p></div>
+            <div id="five2" className="square2"><p>5</p></div>
+            <div id="six2" className="square2"><p>6</p></div>
+            <div id="seven2" className="square2"><p>7</p></div>
+            <div id="eight2" className="square2"><p>8</p></div>
+            <div id="nine2" className="square2"><p>9</p></div>
+            <div id="ten2" className="square2"><p>10</p></div>
           </div>
         </div>
         <button onClick={this.animate.bind(this)}>Go</button>
