167a9cc28885d1effa3642b5a25d22b763428c9d
scottdomes <scottdomes@gmail.com>
2017-02-13 17:22:12 -0800

    Swapped boxes for images

--- src/App.js
+++ src/App.js
@@ -34,12 +34,6 @@ class App extends Component {
         box2: this.getDimensions('two2'),
         box3: this.getDimensions('three2'),
         box4: this.getDimensions('four2'),
-        box5: this.getDimensions('five2'),
-        box6: this.getDimensions('six2'),
-        box7: this.getDimensions('seven2'),
-        box8: this.getDimensions('eight2'),
-        box9: this.getDimensions('nine2'),
-        box10: this.getDimensions('ten2'),
         cont: this.getContainerDimensions(),
       })
     })
@@ -47,6 +41,7 @@ class App extends Component {
 
   getDimensions(id) {
     const cont = document.getElementById('container2')
+    console.log(document.getElementById(id).getBoundingClientRect())
     return {
       top: document.getElementById(id).getBoundingClientRect().top - cont.getBoundingClientRect().top,
       left: document.getElementById(id).getBoundingClientRect().left - cont.getBoundingClientRect().left,
@@ -69,12 +64,6 @@ class App extends Component {
       box2: this.getDimensions('two2'),
       box3: this.getDimensions('three2'),
       box4: this.getDimensions('four2'),
-      box5: this.getDimensions('five2'),
-      box6: this.getDimensions('six2'),
-      box7: this.getDimensions('seven2'),
-      box8: this.getDimensions('eight2'),
-      box9: this.getDimensions('nine2'),
-      box10: this.getDimensions('ten2'),
       cont: this.getContainerDimensions()
     })
   }
@@ -86,28 +75,16 @@ class App extends Component {
         <h1>{text[this.i]}</h1>
         <div id="app">
           <div id="container" style={this.state.cont}>
-            <div id="one" style={this.state.box1} className="square"><p>1</p></div>
-            <div id="two" style={this.state.box2} className="square"><p>2</p></div>
-            <div id="three" style={this.state.box3} className="square"><p>3</p></div>
-            <div id="four" style={this.state.box4} className="square"><p>4</p></div>
-            <div id="five" style={this.state.box5} className="square"><p>5</p></div>
-            <div id="six" style={this.state.box6} className="square"><p>6</p></div>
-            <div id="seven" style={this.state.box7} className="square"><p>7</p></div>
-            <div id="eight" style={this.state.box8} className="square"><p>8</p></div>
-            <div id="nine" style={this.state.box9} className="square"><p>9</p></div>
-            <div id="ten" style={this.state.box10} className="square"><p>10</p></div>
+            <img id="one"  src={require("../../frodo.jpg")} style={this.state.box1} className="square"/>
+            <img id="two"  src={require("../../merry.jpeg")} style={this.state.box2} className="square"/>
+            <img id="three"  src={require("../../pippin.jpg")} style={this.state.box3} className="square"/>
+            <img id="four"  src={require("../../samwise.jpg")} style={this.state.box4} className="square"/>
           </div>
           <div id="container2" className={'adjusted ' + this.state.stage}>
-            <div id="one2" className="square2"><p>1</p></div>
-            <div id="two2" className="square2"><p>2</p></div>
-            <div id="three2" className="square2"><p>3</p></div>
-            <div id="four2" className="square2"><p>4</p></div>
-            <div id="five2" className="square2"><p>5</p></div>
-            <div id="six2" className="square2"><p>6</p></div>
-            <div id="seven2" className="square2"><p>7</p></div>
-            <div id="eight2" className="square2"><p>8</p></div>
-            <div id="nine2" className="square2"><p>9</p></div>
-            <div id="ten2" className="square2"><p>10</p></div>
+            <img id="one2"  src={require("../../frodo.jpg")} className="square2"/>
+            <img id="two2"  src={require("../../merry.jpeg")} className="square2"/>
+            <img id="three2"  src={require("../../pippin.jpg")} className="square2"/>
+            <img id="four2"  src={require("../../samwise.jpg")} className="square2"/>
           </div>
         </div>
         <button onClick={this.animate.bind(this)}>Go</button>
